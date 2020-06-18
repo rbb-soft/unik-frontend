610c7a84a6d26acc90514424788d2b283dd79aa2
Richard Barolin - Autoradio RBB <electronicarbb@yahoo.com.ar>
2020-06-18 15:55:39 -0300

    agregue badge e icono carrito

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import {MatButtonModule} from '@angular/material/button'; 
 import {MatCardModule} from '@angular/material/card';
-import {MatToolbarModule} from '@angular/material/toolbar'; 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon'; 
+import {MatBadgeModule} from '@angular/material/badge'; 
 
 const ModulesMaterial= [
   MatDialogModule,
   MatButtonModule,
   MatCardModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatIconModule,
+  MatBadgeModule
 ];
 
 
